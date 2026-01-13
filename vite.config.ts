5afdb729b712f59c3f9fdb252c4567068b84823b
DarthGigi <47110839+DarthGigi@users.noreply.github.com>
2026-01-13 01:13:45 +0100

    https://www.better-auth.com/docs/reference/faq#dual-module-hazard-issue, https://github.com/better-auth/better-auth/issues/7023#issuecomment-3740635680

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,11 @@
-import devtoolsJson from 'vite-plugin-devtools-json';
-import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
+import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vite';
+import devtoolsJson from 'vite-plugin-devtools-json';
 
-export default defineConfig({ plugins: [tailwindcss(), sveltekit(), devtoolsJson()] });
+export default defineConfig({
+	resolve: {
+		dedupe: ['better-call', 'better-auth', '@better-auth/oauth-provider']
+	},
+	plugins: [tailwindcss(), sveltekit(), devtoolsJson()]
+});
